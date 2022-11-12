675b631dd4929b207c83b786241392f53451fbb6
Drew Layton <dlayton66@gmail.com>
2022-11-12 16:55:28 -0700

    feat/test: Create #encrypted_array and #encrypted_message for Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -2,20 +2,28 @@ require './lib/shifts'
 
 class Enigma
   def initialize
-    @character_set = ("a".."z").to_a << " "
+    @set = ("a".."z").to_a << " "
   end
 
   def encrypt(message,key = nil,date = nil)
-    shifts = Shifts.new(key,date)
-    
-    test = message.chars.map.with_index do |char,i|
-      @character_set[(@character_set.index(char) + shifts.digits[i%4]) % 27]
-    end
-    hash = {
-      date: shifts.date_seed,
-      encryption: test.join(""),
-      key: shifts.seed
+    @key = key
+    @date = date
+    @shifts = Shifts.new(key,date)
+
+    {
+      date: @date,
+      encryption: encrypted_message(message),
+      key: @key
     }
-    hash
+  end
+
+  def encrypted_array(message)
+    message.chars.map.with_index do |char,i|
+      @set[(@set.index(char) + @shifts.digits[i%4]) % @set.size]
+    end
+  end
+
+  def encrypted_message(message)
+    encrypted_array(message).join("")
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -19,4 +19,20 @@ RSpec.describe 'Enigma' do
         expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
     end
   end
+
+  describe '#encrypted_array' do
+    it 'returns an array where each element is an encrypted character' do
+      enigma.encrypt("hello world", "02715", "040895") # just to set seeds
+
+      expect(enigma.encrypted_array("hello world")).to eq ["k", "e", "d", "e", "r", " ", "o", "h", "u", "l", "w"]
+    end
+  end
+
+  describe '#encrypted_message' do
+    it 'returns the encrypted message' do
+      enigma.encrypt("hello world", "02715", "040895") # just to set seeds
+
+      expect(enigma.encrypted_message("hello world")).to eq("keder ohulw")
+    end
+  end
 end
