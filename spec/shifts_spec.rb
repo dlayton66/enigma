c679109881efca987e9b8e1f48bc4b3ec53a1f2b
Drew Layton <dlayton66@gmail.com>
2022-11-12 13:28:28 -0700

    refactor: Give Shifts 2 optional parameters

--- spec/shifts_spec.rb
+++ spec/shifts_spec.rb
@@ -5,7 +5,20 @@ RSpec.describe Shifts do
 
   it 'exists' do
     expect(shifts).to be_a Shifts
-  end  
+  end
+
+  it 'can take 1 argument for master_key' do
+    shifts_one_arg = Shifts.new("02715")
+
+    expect(shifts_one_arg.keys.master_key).to eq "02715"
+  end
+
+  it 'can take 2 arguments for master_key and date' do
+    shifts_two_arg = Shifts.new("02715","040895")
+
+    expect(shifts_two_arg.keys.master_key).to eq "02715"
+    expect(shifts_two_arg.offsets.master_offset).to eq "1025"
+  end
 
   describe '#calculate_shifts' do
     it 'returns an array with keys and offsets added together' do
