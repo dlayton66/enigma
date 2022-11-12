de3496d96c083f5959ec7c94d0eb076b25e68563
Drew Layton <dlayton66@gmail.com>
2022-11-12 14:25:59 -0700

    feat/test: Begin work on #encrypt for Enigma

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,6 +6,14 @@ class Enigma
   end
 
   def encrypt(message,key = nil,date = nil)
+    shifts = Shifts.new(key,date)
+    shifts.digits.map { |digit| digit % 27 }
 
+    alphabet = ("a".."z").to_a << " "
+    
+    test = message.chars.map.with_index do |char,i|
+      alphabet[(alphabet.index(char) + shifts.digits[i%4]) % 27]
+    end
+    require 'pry'; binding.pry
   end
 end
--- spec/enigma_spec.rb
+++ spec/enigma_spec.rb
@@ -1,5 +1,21 @@
 require './lib/enigma'
 
 RSpec.describe 'Enigma' do
-  
+  let (:enigma) { Enigma.new }
+
+  it 'exists' do
+    expect(enigma).to be_a Enigma
+  end
+
+  describe '#encrypt' do
+    it 'encrypts a message with a key and date' do
+        expected = 
+            {
+              encryption: "keder ohulw",
+              key: "02715",
+              date: "040895"
+            }
+        expect(enigma.encrypt("hello world", "02715", "040895")).to eq(expected)
+    end
+  end
 end
