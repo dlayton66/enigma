51d1a58348b2c57c3008426eca00523359528f16
Drew Layton <dlayton66@gmail.com>
2022-11-14 06:09:40 -0700

    refactor: Offsets

--- lib/offsets.rb
+++ lib/offsets.rb
@@ -1,29 +1,29 @@
 class Offsets
-  attr_reader :date_seed,
-              :seed,
+  attr_reader :date,
+              :all,
               :a,
               :b,
               :c,
               :d
 
-  def initialize(date_seed)
-    date_seed ? @date_seed = date_seed : @date_seed = format(Time.now)
-    @seed = calculate_seed(@date_seed)
-    @a = @seed[0]
-    @b = @seed[1]
-    @c = @seed[2]
-    @d = @seed[3]
+  def initialize(date)
+    date.nil? ? @date = format(Time.now) : @date = date
+    @all = find_offsets(@date)
+    @a = @all[0].to_i
+    @b = @all[1].to_i
+    @c = @all[2].to_i
+    @d = @all[3].to_i
   end
   
   def format(date)
     date.strftime("%d%m%y")
   end
 
-  def calculate_seed(date_seed)
-    (date_seed.to_i**2).to_s[-4..-1]
+  def find_offsets(date)
+    (date.to_i**2).to_s[-4..-1]
   end
   
-  def digits
-    [@a.to_i,@b.to_i,@c.to_i,@d.to_i]
-  end
+  # def raw
+  #   [@a.to_i,@b.to_i,@c.to_i,@d.to_i]
+  # end
 end
